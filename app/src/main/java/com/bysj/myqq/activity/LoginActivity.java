ec19cc83222851b84a33c025ee99ddd98feb1c3d
ZhaiNiao <835166582@qq.com>
2018-05-19 00:58:46 +0800

    登录注册界面布局

--- app/src/main/java/com/bysj/myqq/activity/LoginActivity.java
+++ app/src/main/java/com/bysj/myqq/activity/LoginActivity.java
@@ -1,15 +1,52 @@
 package com.bysj.myqq.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bysj.myqq.R;
+import com.bysj.myqq.utils.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_login_phone)
+    EditText etLoginPhone;
+    @BindView(R.id.et_login_psw)
+    EditText etLoginPsw;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.tv_login_forget_psd)
+    TextView tvLoginForgetPsd;
+    @BindView(R.id.tv_login_register)
+    TextView tvLoginRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.tv_login_forget_psd, R.id.tv_login_register})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+            case R.id.tv_login_forget_psd:
+                ToastUtils.showInfo(this,"敬请期待");
+                break;
+            case R.id.tv_login_register:
+                startActivity(new Intent(this,RegistActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/bysj/myqq/activity/RegistActivity.java
+++ app/src/main/java/com/bysj/myqq/activity/RegistActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bysj.myqq.R;
+import com.bysj.myqq.base.BaseActivity;
 
-public class RegistActivity extends AppCompatActivity {
+public class RegistActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+        setBackBtn();
+        setTitle("注册");
     }
 }
